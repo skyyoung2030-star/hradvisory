66478bec74143245db639cf86695f3211d8cf084
skyyoung2030-star <skyyoung2030@gmail.com>
2026-06-17 16:57:02 +0900

    Updated WelcomeMiniChat.tsx

--- src/components/WelcomeMiniChat.tsx
+++ src/components/WelcomeMiniChat.tsx
@@ -181,7 +181,7 @@ export default function WelcomeMiniChat() {
               className="block text-[14px] font-medium mt-1.5"
               style={{ color: "#34d399" }}
             >
-              간단한 궁금증과 자문을 바로 물어보세요 · 평일 업무시간
+              간단한 궁금증과 HR이슈에 대해 간단한 자문을 바로 받아보세요 · 평일 업무시간
             </p>
           </div>
 
