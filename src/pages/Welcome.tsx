eb657bd526be6c4276096264f4146b46b1725e4c
skyyoung2030-star <skyyoung2030@gmail.com>
2026-06-18 10:39:38 +0900

    Updated Welcome.tsx

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -80,7 +80,7 @@ export default function Welcome() {
             베테랑 HR 컨설턴트가 매월 함께하며 조직을 한 단계 성장시켜드립니다.
             <br className="hidden sm:block" />
             <span className="sm:hidden"> </span>
-            5분 인터랙티브 투어로 어떻게 함께하는지 확인해보세요.
+            인력과 관련된 모든 것을 부스팅해드립니다.
           </p>
         </motion.div>
 
